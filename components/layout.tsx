51607b95ffc09bafb7035f8ffd9a2fda35581a05
noops <redtinfoilcrane@gmail.com>
2022-08-23 14:51:28 -0700

    bg test

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,7 @@
 import Alert from './alert'
 import Meta from './meta'
+import Image from 'next/image'
+import styles from './styles.module.css'
 
 type Props = {
   preview?: boolean
@@ -9,6 +11,14 @@ type Props = {
 const Layout = ({ preview, children }: Props) => {
   return (
     <div>
+      <div className={styles["bgWrap"]}>
+        <Image
+          src="/assets/blog/authors/jj.jpeg"
+          layout="fill"
+          objectFit="cover"
+          
+        />
+      </div>
       <Meta />
       <div className="min-h-screen">
         <Alert preview={preview} />
